6d38b18ccb224278273694723083754ea854d9b2
Rixit Dobariya <Tpxitachi05@outlook.com>
2023-12-29 13:26:54 +0530

    q

---
+++ WebApplication/WebForm10.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication
+{
+    public partial class WebForm10 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(Request.Cookies["stud"]!=null)
+            {
+                string val= Request.Cookies["stud"].Value;
+                Label1.Text = val;
+            }
+        }
+    }
+}
---
+++ WebApplication/WebForm9.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication
+{
+    public partial class WebForm9 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            HttpCookie stud = new HttpCookie("Student");
+            stud.Value = txtValue.Text;
+
+            stud.Expires = DateTime.Now.AddHours(1);
+
+            Response.Cookies.Add(stud);
+            Response.Redirect("WebForm10.aspx");
+        }
+    }
+}
